9df76b4663813eea49bfd0c54b0a33aab087ac51
Tabirta Adrian <adriantabirta@gmail.com>
2021-08-23 15:48:32 +0300

    Initial Commit

---
+++ alar-studios-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alar-studios-test
+//
+//  Created by Tabirta Adrian on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
